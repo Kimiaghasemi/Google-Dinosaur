d73d44def4fd3402f409bb5f8808dd5b95c1e19e
Hossein <djalicrt@gmail.com>
2020-10-24 18:17:28 +0330

    All of Project is done!

--- app.js
+++ app.js
@@ -1,38 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
 const dino = document.querySelector('.dino')
+const grid = document.querySelector('.grid')
+const alert = document.getElementById('alert')
+let isJumping = false
+let gravity = 0.9
+let isGameOver = false
 
 function control(e) {
-
+    //https://keycode.info/
     if (e.keyCode === 32) {
-       jump()
+        if (!isJumping) {
+            isJumping = true
+            jump()
+        }
+        
     }
 }
 
 document.addEventListener('keyup', control)
 
-function jump() {
 let position = 0
+
+function jump() {
+let count = 0
 let timerId = setInterval(function () {
 
     //move down
-    if (position === 150) {
+    if (count === 15) {
         clearInterval(timerId)
         console.log('down')
         let downTimerId = setInterval(function() {
-            if (position === 0) {
+            if (count === 0) {
                 clearInterval(downTimerId)
+                isJumping = false
             }
-            position -= 30
+            position -= 5
+            count--
+            position = position * gravity
             dino.style.bottom = position + 'px'
         },20)
     }
     //move up
     console.log('up')
     position += 30
+    count++
+    position = position * gravity
     dino.style.bottom = position + 'px'
+    console.log(dino.style.bottom)
 }, 20)
-
 }
 
-
+function generateCactus() {
+    let randomTime = Math.random() * 4000 
+    let cactusPostion = 1000
+    const cactus = document.createElement('div')
+    if (!isGameOver) cactus.classList.add('cactus')
+    grid.appendChild(cactus)
+    cactus.style.left = cactusPostion + 'px'
+    
+    let timerId = setInterval(function() {
+        if (cactusPostion === 0 && cactusPostion < 60 && position < 60) {
+            clearInterval(timerId)
+            alert.innerHTML = 'Game Over'
+            isGameOver = true
+            while (grid.firstChild) {
+                grid.removeChild(grid.lastChild)
+            }
+        }
+        cactusPostion -=10
+        cactus.style.left = cactusPostion + 'px'
+    },20)
+    if (!isGameOver) setTimeout(generateCactus, randomTime)
+}
+ generateCactus()
 })
